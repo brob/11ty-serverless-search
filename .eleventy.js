29f262e5b2393ceed504165dcdb76d2b6a960e4b
Bryan Robinson <bryanlrobinson@gmail.com>
2021-09-22 09:25:46 -0400

    Fixes anchor closing to button closing

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,8 @@
 require("dotenv").config();
-const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
-const algoliasearch = require("algoliasearch");
-
-const client = algoliasearch(process.env.ALGOLIA_APP, process.env.ALGOLIA_SEARCH_KEY);
-const index = client.initIndex(process.env.ALGOLIA_INDEX);
 
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-    name: "search", // The serverless function name from your permalink object
-    functionsDir: "./netlify/functions/",
-  });
+  // Configuration rules
 
-  eleventyConfig.addFilter("getResults", function(query) {
-      console.log(query);
-    const results = index.search(query, {
-        attributesToRetrieve: ["title", "url", "date", "description"],
-
-    }).then(res => {
-        return res.hits;
-    })
-    return results
-   });
 
 };
